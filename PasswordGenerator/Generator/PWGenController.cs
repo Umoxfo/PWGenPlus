e69636325207100fdf5f8c3fa4d9490d5a9bd79b
Umoxfo <ycco34vx@gmail.com>
2019-06-05 15:19:08 -0700

    Separate the section that generates a random string into a separate class

--- PasswordGenerator/Generator/PWGenController.cs
+++ PasswordGenerator/Generator/PWGenController.cs
@@ -20,7 +20,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 
 using Umoxfo.Security.Password.Settings;
 
@@ -28,7 +27,7 @@ namespace Umoxfo.Security.Password.Generator
 {
     public sealed class PWGenController
     {
-        private static readonly RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+        private static readonly PasswordGenerator passGen = new PasswordGenerator();
 
         /// <summary>
         /// Generate a list of passwords
@@ -50,80 +49,16 @@ namespace Umoxfo.Security.Password.Generator
                 string pwd;
                 do
                 {
-                    pwd = GetRandomString(characterSet, length, passwordEncoding);
+                    pwd = passGen.GetPassword(characterSet, length, passwordEncoding);
                 }
                 while (!duplicateCheck.Add(pwd));
 
                 passwordList.Add(new Password(pwd));
             }//for
 
-            rngCsp.Dispose();
+            passGen.Dispose();
 
             return passwordList;
         }//GeneratePasswords
-
-        /// <summary>
-        /// Generate a random string
-        /// </summary>
-        /// <param name="characterArray">The character-set that the generator can use</param>
-        /// <param name="length">The length of the string that needs to be generated</param>
-        /// <param name="passwordEncoding">The encoding of the raw password used when converting to be a string</param>
-        /// <returns>Generated password string</returns>
-        private static string GetRandomString(in char[] characterArray, int length, in PasswordEncoding passwordEncoding)
-        {
-            byte[] buf = new byte[length];
-            rngCsp.GetBytes(buf);
-
-            switch (passwordEncoding)
-            {
-                case PasswordEncoding.Base64: return Convert.ToBase64String(buf).Remove(length);
-                case PasswordEncoding.HexLower: return BitConverter.ToString(buf, 0, length / 2).Replace("-", "").ToLower();
-                case PasswordEncoding.HexUpper: return BitConverter.ToString(buf, 0, length / 2).Replace("-", "");
-                default:
-                    char[] result = new char[length];
-
-                    int[] previousValueIndexs = new int[2];
-                    for (int i = 0; i < length; i++)
-                    {
-                        int valueIndex = RollDice(characterArray.Length);
-
-                        // Not more than 2 identical characters in a row
-                        if ((previousValueIndexs[0] == previousValueIndexs[1]) && (previousValueIndexs[1] == valueIndex))
-                        {
-                            i--;
-                            continue;
-                        }//if
-
-                        previousValueIndexs[0] = previousValueIndexs[1];
-                        previousValueIndexs[1] = valueIndex;
-
-                        result[i] = characterArray[valueIndex];
-                    }//for
-
-                    return new string(result);
-            }//switch
-        }//GetRandomString
-
-        private static int RollDice(int numberSides)
-        {
-            if (numberSides <= 0) throw new ArgumentOutOfRangeException("Invalid character-set");
-
-            // The maximum value in the full sets of the dice sides that can come up in an int32.
-            int maxValueOfFullSets = (int.MaxValue / numberSides) * numberSides;
-
-            // Create a byte array to hold the random value.
-            byte[] randomNumber = new byte[4];
-            uint value;
-            do
-            {
-                // Fill the array with a random value.
-                rngCsp.GetBytes(randomNumber);
-                value = BitConverter.ToUInt32(randomNumber, 0);
-            }
-            while (value >= maxValueOfFullSets);
-
-            // The possible values are zero-based.
-            return (int)(value % numberSides);
-        }//RollDice
     }
 }
---
+++ PasswordGenerator/Generator/PasswordGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Security.Cryptography;
+using Umoxfo.Security.Password.Settings;
+
+namespace Umoxfo.Security.Password.Generator
+{
+    public sealed class PasswordGenerator
+    {
+        private readonly RNGCryptoServiceProvider rngCsp;
+
+        public PasswordGenerator() => rngCsp = new RNGCryptoServiceProvider();
+
+        /// <summary>
+        /// Generate a random string
+        /// </summary>
+        /// <param name="characterArray">The character-set that the generator can use</param>
+        /// <param name="length">The length of the string that needs to be generated</param>
+        /// <param name="passwordEncoding">The encoding of the raw password used when converting to be a string</param>
+        /// <returns>Generated password string</returns>
+        public string GetPassword(in char[] characterArray, int length, in PasswordEncoding passwordEncoding)
+        {
+            byte[] buf = new byte[length];
+            rngCsp.GetBytes(buf);
+
+            switch (passwordEncoding)
+            {
+                case PasswordEncoding.Base64: return Convert.ToBase64String(buf).Remove(length);
+                case PasswordEncoding.HexLower: return BitConverter.ToString(buf, 0, length / 2).Replace("-", "").ToLower();
+                case PasswordEncoding.HexUpper: return BitConverter.ToString(buf, 0, length / 2).Replace("-", "");
+                default:
+                    char[] result = new char[length];
+
+                    int[] previousValueIndexs = new int[2];
+                    for (int i = 0; i < length; i++)
+                    {
+                        int valueIndex = RollDice(characterArray.Length);
+
+                        // Not more than 2 identical characters in a row
+                        if ((previousValueIndexs[0] == previousValueIndexs[1]) && (previousValueIndexs[1] == valueIndex))
+                        {
+                            i--;
+                            continue;
+                        }//if
+
+                        previousValueIndexs[0] = previousValueIndexs[1];
+                        previousValueIndexs[1] = valueIndex;
+
+                        result[i] = characterArray[valueIndex];
+                    }//for
+
+                    return new string(result);
+            }//switch
+        }//GetPassword
+
+        private int RollDice(int numberSides)
+        {
+            if (numberSides <= 0) throw new ArgumentOutOfRangeException("Invalid character-set");
+
+            // The maximum value in the full sets of the dice sides that can come up in an int32.
+            int maxValueOfFullSets = (int.MaxValue / numberSides) * numberSides;
+
+            // Create a byte array to hold the random value.
+            byte[] randomNumber = new byte[4];
+            uint value;
+            do
+            {
+                // Fill the array with a random value.
+                rngCsp.GetBytes(randomNumber);
+                value = BitConverter.ToUInt32(randomNumber, 0);
+            }
+            while (value >= maxValueOfFullSets);
+
+            // The possible values are zero-based.
+            return (int)(value % numberSides);
+        }//RollDice
+
+        public void Dispose() => rngCsp.Dispose();
+    }
+}
