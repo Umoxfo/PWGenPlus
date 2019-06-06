3485f4b915d16cc3bee1fa2c8f24d1e028057a48
Umoxfo <ycco34vx@gmail.com>
2019-06-05 21:28:43 -0700

    Add support the Diceware

---
+++ PasswordGenerator/Generator/Diceware.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace Umoxfo.Security.Password.Generator
+{
+    class Diceware
+    {
+        private static readonly ReadOnlyDictionary<string, string> dicewareWordList;
+
+        static Diceware()
+        {
+            Dictionary<string, string> tmp = new Dictionary<string, string>();
+
+            // Read a word list file
+            using (StreamReader sr = new StreamReader(@".\words\dicewarewordlist.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] values = sr.ReadLine().Split(',');
+
+                    Console.WriteLine($"{values[0]} { values[1]}");
+                    tmp.Add(values[0], values[1]);
+                }//while
+            }
+
+            dicewareWordList = new ReadOnlyDictionary<string, string>(tmp);
+        }//Diceware()
+
+        public static void Test()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
