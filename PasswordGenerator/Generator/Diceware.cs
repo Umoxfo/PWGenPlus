e4f06db047116c2db0d0fd2ca6e76c1d9bef5c86
Umoxfo <ycco34vx@gmail.com>
2019-06-09 15:08:26 -0700

    implement Diceware

--- PasswordGenerator/Generator/Diceware.cs
+++ PasswordGenerator/Generator/Diceware.cs
@@ -2,22 +2,27 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Umoxfo.Security.Password.Generator
 {
     internal class Diceware
     {
-        private static readonly char[,] ReplacementTable = new char[6, 6]
+        private const byte DiceSides = 6;
+        private const int WordLength = 7;
+
+        private static readonly RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+        private static readonly string[,] ReplacementTable = new string[6, 6]
         {
            // Therd (3rd) Roll
            // 1    2    3    4    5    6
- /* F  1 */ {'`', '!', '#', '$', '%', 'ˆ'},
- /* o  2 */ {'&', '*', '(', ')', '-', '='},
- /* u  3 */ {'+', '[', ']', '\\', '{', '}'},
- /* r  4 */ {':', ';', '"', '\'', '<', '>'},
- /* t  5 */ {'?', '/', '0', '1', '2', '3'},
- /* h  6 */ {'4', '5', '6', '7', '8', '9'}
+ /* F  1 */ {"~", "!", "#", "$", "%", "ˆ"},
+ /* o  2 */ {"&", "*", "(", ")", "-", "="},
+ /* u  3 */ {"+", "[", "]", @"\", "{", "}"},
+ /* r  4 */ {":", ";", "\"", "'", "<", ">"},
+ /* t  5 */ {"?", "/", "0", "1", "2", "3"},
+ /* h  6 */ {"4", "5", "6", "7", "8", "9"}
         };
 
         private readonly ReadOnlyDictionary<string, string> dicewareWordList;
@@ -43,6 +48,78 @@ namespace Umoxfo.Security.Password.Generator
 
             dicewareWordList = new ReadOnlyDictionary<string, string>(tmp);
         }//Diceware(string wordListFilePath)
+
+        internal string GetPassphrase(int numberWords, bool extraSecurity)
+        {
+            string[] wordKeys = GenerateKeys(numberWords);
+
+            StringBuilder stringBuilder = new StringBuilder(WordLength * numberWords);
+            foreach (string key in wordKeys)
+            {
+                dicewareWordList.TryGetValue(key, out string phrase);
+
+                /*
+                if (extraSecurity)
+                {
+                    phrase = ToSecurePhrase(phrase);
+                }//if
+                */
+
+                stringBuilder.Append(phrase).Append(" ");
+            }//foreach
+
+            return stringBuilder.ToString().Trim();
+        }//GetPassphrase
+
+        private static string[] GenerateKeys(int numWords)
+        {
+            string[] wordKeys = new string[numWords];
+
+            for (int i = 0; i < numWords; i++)
+            {
+                int wordKey = 0;
+
+                // Roll the dice 5 times
+                for (int r = 1; r < DiceSides; r++)
+                {
+                    wordKey = (wordKey << 8) | RollDice(DiceSides);
+                }//for
+
+                // Add new number to array of wordKeys
+                wordKeys[i] = Convert.ToString(wordKey);
+            }
+
+            return wordKeys;
+        }//GenerateKeys
+
+/*
+        private static string ToSecurePhrase(string word)
+        {
+            // Choose a letter in the word to replace
+            int insertIndex = RollDice(DiceSides) - 1;
+
+            // Pick the added character from the replacement table
+            string addedLetter = ReplacementTable[RollDice(DiceSides) - 1, RollDice(DiceSides) - 1];
+
+            return word.Insert(insertIndex, addedLetter);
+        }//ToSecurePhrase
+*/
+
+        private static byte RollDice(byte numberSides)
+        {
+            // The maximum value in the full sets of the dice sides that can come up in a byte.
+            int maxValueOfFullSets = (byte.MaxValue / numberSides) * numberSides;
+
+            byte[] randomNumber = new byte[1];
+            do
+            {
+                rngCsp.GetBytes(randomNumber);
+            }
+            while (randomNumber[0] >= maxValueOfFullSets);
+
+            return (byte)((randomNumber[0] % numberSides) + 1);
+        }//RollDice
+
 
         /*        public  void Test()
                 {
