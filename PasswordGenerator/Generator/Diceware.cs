6f2088d50a4bf735975766be6525e87e77107b77
Umoxfo <ycco34vx@gmail.com>
2019-06-16 12:36:31 -0700

    Update copyright year

--- PasswordGenerator/Generator/Diceware.cs
+++ PasswordGenerator/Generator/Diceware.cs
@@ -1,9 +1,27 @@
-﻿using System;
+﻿/*
+   PasswordGenerator
+   Copyright (c) 2019-2019 Makoto Sakaguchi <ycco34vx@gmail.com>
+
+   This file is part of PasswordGenerator.
+
+   PasswordGenerator is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   PasswordGenerator is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with PasswordGenerator.  If not, see <https://www.gnu.org/licenses/>.
+ */
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Umoxfo.Security.Password.Generator
--- PasswordGenerator/Settings/PasswordSettings.cs
+++ PasswordGenerator/Settings/PasswordSettings.cs
@@ -1,22 +1,22 @@
-﻿/* PasswordGenerator
- * Copyright (c) 2018-2018 Makoto Sakaguchi <ycco34vx@gmail.com>
- *
- * This file is part of PasswordGenerator.
- *
- * PasswordGenerator is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PasswordGenerator is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PasswordGenerator.  If not, see <https://www.gnu.org/licenses/>.
- */
+﻿/*
+   PasswordGenerator
+   Copyright (c) 2018-2019 Makoto Sakaguchi <ycco34vx@gmail.com>
 
+   This file is part of PasswordGenerator.
+
+   PasswordGenerator is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   PasswordGenerator is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with PasswordGenerator.  If not, see <https://www.gnu.org/licenses/>.
+ */
 namespace Umoxfo.Security.Password.Settings
 {
     public enum PasswordEncoding
